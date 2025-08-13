3f1bbbc8ec834813b681dc9a480c9834e0989407
ashkan-101 <ashkan.dev.101@gmail.com>
2025-08-13 21:05:35 +0330

    create signal.schema

---
+++ consumer/src/modules/signal/schema/signal.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema({ collection: 'signals', timestamps: true })
+export class Signal {
+  @Prop({ required: true })
+  deviceId: string;
+
+  @Prop({ required: true })
+  timestamp: number;
+
+  @Prop({ required: true })
+  dataLength: number;
+
+  @Prop({ 
+    type: [{ 
+      time: Number, 
+      coordinates: [Number] 
+    }], 
+    required: true 
+  })
+  readings: { time: number; coordinates: number[] }[];
+}
+
+export type SignalDocument = HydratedDocument<Signal>
+export const SignalSchema = SchemaFactory.createForClass(Signal);
