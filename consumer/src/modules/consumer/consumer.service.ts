c415ba1555619b0b71a7258a9b4efc91ca5f8395
ashkan-101 <ashkan.dev.101@gmail.com>
2025-08-13 20:43:23 +0330

    create processed-raw-data in consumer-service

--- consumer/src/modules/consumer/consumer.service.ts
+++ consumer/src/modules/consumer/consumer.service.ts
@@ -1,24 +1,28 @@
 import { Injectable } from "@nestjs/common";
+import { IProcessedSignal } from "src/common/interface/IProcessedSignal";
 
 @Injectable()
 export class ConsumerService {
 
-  public async processData(rawData: any) {
-  const result: any[] = [];
+  async processRawData(rawData: any) {
+    const processedSignals: IProcessedSignal[] = [];
 
-  for (const deviceId in rawData) {
-    const deviceData = rawData[deviceId];
-    const readings = deviceData.data.map((entry: any[]) => ({
-    time: entry[0],
-     coordinates: entry[1],
-    }));
+    for (const deviceId in rawData) {
+      const deviceData = rawData[deviceId];
+      const readings = deviceData.data.map((entry: any[]) => ({
+        time: entry[0], 
+        coordinates: entry[1],    
+      }));
 
-    result.push({
-    deviceId,
-    timestamp: deviceData.time,
-    readings,
-     });
-     }
-     return result;
+      processedSignals.push({
+        deviceId,
+        timestamp: deviceData.time, 
+        dataLength: readings.length,
+        readings,
+      });
+    }
+
+    console.log(processedSignals);
+    // return processedSignals;
   }
 }
