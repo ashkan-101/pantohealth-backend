2fbf6dff1a3bc145674c8ec08cc95145ed0f1b79
ashkan-101 <ashkan.dev.101@gmail.com>
2025-08-13 16:31:08 +0330

    config microservice in main and created app.module

--- consumer/src/app.module.ts
+++ consumer/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: join(process.cwd(), '.env')
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI as string)
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- consumer/src/main.ts
+++ consumer/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RmqOptions, Transport } from '@nestjs/microservices'
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice(AppModule,{
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
+      queue: process.env.RABBITMQ_QUEUE || 'x-ray-queue',
+      queueOptions: { durable: true }
+    }
+  } as RmqOptions);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true}))
+  
+  await app.listen();
+  console.log('consumer microservice is running ... ');
 }
 bootstrap();
